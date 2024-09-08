66cbc39b16ed675007f97a599ca6910f209f7778
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-09-08 23:44:25 +0530

    Create l6q2.c

---
+++ Lab6/l6q2.c
@@ -0,0 +1,28 @@
+//if switch closed(pressed) -> 0 in P2.12 -> 1 in LED
+//           else(grounded) -> 1 in P2.12 -> 0 in LED
+
+//HAVE TO CHECK IF WORKS
+
+#include <LPC17xx.h>
+
+int main()
+{
+	int x;
+	SystemInit();
+	SystemCoreClockUpdate();
+	LPC_PINCON->PINSEL0=0; //PO.4
+	LPC_PINCON->PINSEL4=0; //P2.12
+	LPC_GPIO0->FIODIR=1<<4; //For LED
+	LPC_GPIO2->FIODIR=0<<12; //For Switch
+	
+	//Reading value of port 2 to x
+	x=LPC_GPIO2->FIOPIN;
+	x=x&1 << 12;
+	while(1)
+	{
+		if(x==0)
+			LPC_GPIO0->FIOSET=1<<4;
+		else
+			LPC_GPIO0->FIOCLR=1<<4;
+	}
+}
