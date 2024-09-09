5377432a7078462263a6e35b48f06e34ef06cf44
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-09-09 09:21:29 +0530

    Create l7q1.c

---
+++ Lab7/l7q1.c
@@ -0,0 +1,19 @@
+#include <LPC17xx.h>
+
+int main()
+{
+	int i,j;
+	SystemInit();
+	SystemCoreClockUpdate();
+	LPC_PINCON->PINSEL0=0;
+	LPC_GPIO0->FIODIR=0XF<<4;
+	while(1)
+	{
+		for(i=0;i<=255;i++)
+		{
+			LPC_GPIO0->FIOPIN=i<<4;
+			for(j=0;j<10000;j++);//delay
+		}
+	}
+	
+}
