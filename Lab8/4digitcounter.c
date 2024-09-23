0b924d507ddce7d5cd5d3d7abd1f00afdf54d812
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-09-23 17:09:16 +0530

    Create 4digitcounter.c

---
+++ Lab8/4digitcounter.c
@@ -0,0 +1,36 @@
+//works
+//Static 4 digit number
+//Static number
+#include <LPC17xx.h>
+int main()
+{
+	int seven_seg[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+	int n=0,temp,count=0,i,j;
+	SystemInit();
+	SystemCoreClockUpdate();
+
+	LPC_PINCON->PINSEL0=0;//Configuring lines P0.11 to P0.4+
+	LPC_PINCON->PINSEL3=0;//P1.26 to P1.23 for decoder
+
+LPC_GPIO0->FIODIRL=0XFF0;
+LPC_GPIO1->FIODIR=0xF<<23;
+
+while(1)
+{
+	temp=n;
+	if(count==1000)
+	{
+		n=(n+1)%10000;
+		count=0;
+	}
+	for(i=0;i<4;i++)
+	{
+		LPC_GPIO1->FIOPIN=i<<23;//for decoder
+		LPC_GPIO0->FIOPIN=seven_seg[temp%10]<<4;
+		temp=temp/10;
+		for(j=0;j<10000;j++);//delay
+		count++;
+	}
+}
+	
+}
